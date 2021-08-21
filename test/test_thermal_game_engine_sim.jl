506b4c59234c4525726aaf36be0bda380cd25f68
Varnerlab <jdv27@cornell.edu>
2021-08-21 18:05:54 -0400

    Some new test
    New tests

--- test/test_thermal_game_engine_sim.jl
+++ test/test_thermal_game_engine_sim.jl
@@ -4,7 +4,7 @@ using DataFrames
 using Plots
 
 path_to_game_world = "/Users/jeffreyvarner/Desktop/julia_work/VLMinorityGameKit.jl/test/data/GW-T-M10-S5-NA1001.bson"
-path_to_results_dir = "/Users/jeffreyvarner/Desktop/julia_work/VLMinorityGameKit.jl/test/data/results/SIM-T-M10-S5-NA1001-L1.bson"
+path_to_results_dir = "/Users/jeffreyvarner/Desktop/julia_work/VLMinorityGameKit.jl/test/data/results/SIM-T-M10-S5-NA1001-L2.bson"
 d = BSON.load(path_to_game_world)
 gameWorld = d[:gameworld]
 
@@ -17,7 +17,7 @@ numberOfTraders = 1001
 price_array = Array{Float64,2}(undef, (numberOfTimeSteps), number_of_samples)
 simulation_result_array = Array{DataFrame,1}(undef, number_of_samples)
 for sample_index = 1:number_of_samples
-    result = execute(gameWorld, numberOfTimeSteps; liquidity=1.0 * numberOfTraders);
+    result = execute(gameWorld, numberOfTimeSteps; liquidity=2.0 * numberOfTraders);
     simulation_result_array[sample_index] = result.data
     println("Completed sample = $(sample_index) ...")
 end
@@ -32,7 +32,7 @@ simulation_dictionary["number_of_timesteps"] = number_of_table_rows
 simulation_dictionary["number_of_traders"] = numberOfTraders
 simulation_dictionary["memory"] = 10
 simulation_dictionary["strategies_per_agent"] = 5
-simulation_dictionary["liquidity"] = 1.0 * numberOfTraders
+simulation_dictionary["liquidity"] = 2.0 * numberOfTraders
 simulation_dictionary["simulation_result_array"] = simulation_result_array
 
 # dump -
